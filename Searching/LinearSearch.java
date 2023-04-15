94e06f0a9b25616dd499dd9016f3f50db029222b
Manaswini Verma <85567107+winnie-0-0@users.noreply.github.com>
2023-04-15 19:47:23 +0000

    Linear Search Q1

---
+++ Searching/LinearSearch.java
@@ -0,0 +1,46 @@
+// LINEAR SEARCH 
+/* https://leetcode.com/problems/find-numbers-with-even-number-of-digits/ 
+ 
+
+Input: nums = [12,345,2,6,7896]
+Output: 2
+Explanation: 
+12 contains 2 digits (even number of digits). 
+345 contains 3 digits (odd number of digits). 
+2 contains 1 digit (odd number of digits). 
+6 contains 1 digit (odd number of digits). 
+7896 contains 4 digits (even number of digits). 
+Therefore only 12 and 7896 contain an even number of digits.
+
+ */
+import java.util.Arrays;
+
+ class Solution {
+
+    public static void main(String[] args){
+
+        int[] nums = {23,67,88889,3,000,9,8};
+        int count = findNumbers(nums);
+        System.out.println("\nThe number of integers with Even digits in given array : " + Arrays.toString(nums) + " is = " + count);
+
+    }
+    static int findNumbers(int[] nums) {
+        int count = 0;
+        for(int ele : nums){
+            int n = ele;
+            int digits = 0;
+            while (n > 0){
+                n = (n - n%10)/10; 
+                // can also be simply written as n/10
+                digits ++;
+            }
+
+            if(digits%2 == 0){
+                count++;
+            }
+        }
+
+        return count;
+        
+    }
+}
