6ed871976d775a2471b388db44fd2d97a3c1a276
Manaswini Verma <85567107+winnie-0-0@users.noreply.github.com>
2023-04-27 11:48:22 +0000

    ceil and floor of a number

---
+++ Searching/BinarySearch/ceilingOrFloorOfaNumber.java
@@ -0,0 +1,89 @@
+package BinarySearch;
+
+public class ceilingOrFloorOfaNumber {
+    public static void main(String[] args){
+
+        int[] arr = {-2, 3, 45, 67, 88 ,999, 8000, 54637};
+
+        int ans = ceilOrFloor(arr, 450, 1);
+        int ans1 = ceilOrFloor(arr, 450, 0);
+        System.out.println("\nCeiling of input number : " + Integer.toString(arr[ans]));
+        System.out.println("\nFloor of input number : " + Integer.toString(arr[ans1]));
+
+    }
+
+    static int ceilOrFloor(int[] arr, int target, int cF){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+            //calculating mid 
+            int mid = start + (end - start)/2 ;
+            //checking if target > , = , < mid
+            if(target == arr[mid]){
+                return mid ;
+            }
+
+            if(target > arr[mid]){
+                start = mid +1;
+            }
+
+            if(target < arr[mid]){
+                end = mid - 1;
+            }
+        }
+
+        if(cF == 1){
+            return start;
+        }else{
+            return end;
+        }
+        
+
+    }
+}
+
+/*
+ For descending order : 
+
+ package BinarySearch;
+
+public class basicBinarySearchAlgo {
+    public static void main(String[] args){
+
+        int[] arr = {-2, 3, 45, 67, 88 ,999, 8000, 54637};
+
+        int ans = binarySearch(arr, 450);
+
+        System.out.println("\nBinary Searched and found target at index : " + Integer.toString(ans));
+
+    }
+
+    static int binarySearch(int[] arr, int target){
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+            //calculating mid 
+            int mid = start + (end - start)/2 ;
+            //checking if target > , = , < mid
+            if(target == arr[mid]){
+                return mid ;
+            }
+
+            if(target > arr[mid]){
+                end = mid - 1;
+            }
+
+            if(target < arr[mid]){
+                start = mid +1;
+            }
+        }
+
+        return -1;
+
+    }
+}
+
+
+ */
