ddf92f584eb954194b895457492f3ffb36784c78
Manaswini Verma <85567107+winnie-0-0@users.noreply.github.com>
2023-04-28 18:39:11 +0000

    update on leetcode 34 binary search

--- Searching/BinarySearch/First_and_Last_position_of_a_Num.java
+++ Searching/BinarySearch/First_and_Last_position_of_a_Num.java
@@ -1,16 +1,26 @@
 package BinarySearch;
+/*
+LEETCODE 34.
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+If target is not found in the array, return [-1, -1].
+You must write an algorithm with O(log n) runtime complexity. 
+
+link : https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+*/
+import java.util.*;
 
 public class First_and_Last_position_of_a_Num{
     public static void main(String[] args) {
-
-        System.out.println(searchRange(null, 0));
+        int[] arr1 = {5,5,7,7,7,7,8,8,9};
+        
+        System.out.println(Arrays.toString(searchRange(arr1, 7)));
         
     }
 
     static int[] searchRange(int[] arr, int target){
         int[] ans = {-1,-1};
-        int start = search(ans, target, true);
-        int end = search(ans, target, false);
+        int start = search(arr, target, true);
+        int end = search(arr, target, false);
 
         ans[0] = start;
         ans[1] = end;
@@ -21,30 +31,34 @@ public class First_and_Last_position_of_a_Num{
     static int search(int[] nums, int target, boolean startIndSearch){
 
         int start = 0;
-        int end = arr.length - 1;
+        int end = nums.length - 1;
         int ans = -1;
         while(start <= end){
             //calculating mid 
             int mid = start + (end - start)/2 ;
             //checking if target > , = , < mid
-            if(target == arr[mid]){
+            if(target == nums[mid]){
+                
                 ans = mid ;
-                if(startIndSerach){
+
+                if(startIndSearch){
                     end = mid - 1;
                 }else{
                     start = mid + 1;
                 }
+
+                
             }
 
-            if(target > arr[mid]){
+            if(target > nums[mid]){
                 start = mid +1;
             }
 
-            if(target < arr[mid]){
+            if(target < nums[mid]){
                 end = mid - 1;
             }
         }
 
-        return ans;
+        return ans; 
     }
 }
