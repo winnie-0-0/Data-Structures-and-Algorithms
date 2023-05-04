e34e930e85f2fc3c2d572ed96839f54a379795a3
Manaswini Verma <85567107+winnie-0-0@users.noreply.github.com>
2023-05-04 15:18:50 +0000

    rotated binary search with dupe elements

--- Searching/BinarySearch/rotated_binary_search.java
+++ Searching/BinarySearch/rotated_binary_search.java
@@ -2,8 +2,8 @@ package BinarySearch;
 
 public class rotated_binary_search {
     public static void main(String[] args) {
-        int[] arr = {3,5,1};
-        System.out.println((Search(arr, 3)));
+        int[] arr = {2,9,1,2,2,2};
+        System.out.println((Search(arr, 1)));
         
     }
 
@@ -80,5 +80,53 @@ public class rotated_binary_search {
 
         return -1;
 
+    }
+
+    static int findDupPivot(int[] arr, int target){
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start <= end){
+            int mid =  start + (end - start)/2;
+            //basic 4 cases
+            //if(arr[mid] > arr[mid + 1]) gives index out of bound exception for mid == end hence this should be checked only when mid < end
+            if(mid < end && arr[mid] > arr[mid + 1]){
+                return mid;
+            }
+
+            if (mid > start && arr[mid] < arr[mid - 1]){
+                return mid - 1;
+            }
+
+            if(arr[start] > arr[mid]){
+                end = mid - 1;
+            }else {
+                start = mid + 1;
+            }
+
+            // when mid = start = end 
+            if(arr[mid] == arr[start] && arr[mid] == arr[end] ){
+                //skip dupes
+                if(arr[start] > arr[start +1] ){
+                    return start;
+                }
+                start++;
+
+                if(arr[end] < arr[end - 1]){
+                    return end - 1;
+                }
+                end--;
+                // what if these were pivots ? solved above
+            } else if (arr[start] < arr[mid] || arr[start] == arr[mid] && arr[mid] > arr[end]){
+                start = mid + 1;
+            } else {
+                end = mid -1;
+            }
+
+
+        }   
+
+        return -1;
     }
 }
